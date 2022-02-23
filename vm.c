d4edd93930ee70aa36d7c1a71a5a5d1c928a9b74
Grayson Crozier <graysoncrozier40@gmail.com>
2022-02-22 19:29:48 -0500

    Add fetch-execute cycle

--- vm.c
+++ vm.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "compiler.h"
+#include "vm.h"
 #define REG_FILE_SIZE 10
 #define MAX_STACK_LENGTH 100
 
@@ -43,6 +44,153 @@ int base(int L, int BP, int *stack)
 
 void execute_program(instruction *code, int printFlag)
 {
+  // Initialize base pointer, stack pointer, and program counter.
+  int BP = MAX_STACK_LENGTH - 1;
+  int SP = BP + 1;
+  int PC = 0;
+
+  // Enter fetch-execute cycle.
+  int halt = 0;
+  while (!halt)
+  {
+    // Fetch an instruction and place in IR register
+    // Increment program counter
+    instruction IR;
+    IR = code[PC++];
+
+    // Execute instruction based off instruction opcode
+    switch (IR.opcode)
+    {
+    default:
+      printf("bad opcode");
+      break;
+
+    case LIT:
+      // RF[IR.R] = IR.M
+      break;
+
+    case RET:
+      // Return from current procedure (X) to the last procedure (Y).
+      // SP = the index of the end of Y’s AR (BP + 1)
+      // BP = dynamic link value from X’s AR
+      // PC = return address value from X’s AR
+      break;
+
+    case LOD:
+      // Load value to register IR.R from the stack location at
+      // offset RF[IR.M] from L lexicographical levels up
+      // RF[IR.R] = stack[base(L) - RF[IR.M]]
+      // Before performing the load, check for Out of Bounds
+      // Access Error by checking if base(L) - M is less than
+      // zero or greater than or equal to MAX_STACK_LENGTH
+      break;
+
+    case STO:
+      // Store value from register IR.R to the stack location at
+      // offset RF[IR.M] from L lexicographical levels down
+      // stack[base(L) - RF[IR.M]] = RF[IR.R]
+      // Before performing the load, check for Out of Bounds
+      // Access Error by checking if base(L) - M is less than
+      // zero or greater than or equal to MAX_STACK_LENGTH
+      break;
+
+    case CAL:
+      // Call procedure at code index M. This will generate a
+      // new Activation Record. There are three values in the
+      // AR:
+      //  1st - static link = base(L)
+      //  2nd - dynamic link = BP
+      //  3rd - return address = PC
+      // After creating the activation record,
+      //  BP = the index of the first entry of the new AR
+      //  PC = IR.M
+      break;
+
+    case INC:
+      // Decrement SP by M, check for Stack Overflow Error
+      // which can occur if SP < 0 after the decrement
+      break;
+
+    case JMP:
+      // Jump to instruction M
+      break;
+
+    case JPC:
+      // Jump to instruction M if register R is 0
+      break;
+
+    case WRT:
+      // Print register R
+      break;
+
+    case RED:
+      // Register R equals scanf()
+      break;
+
+    case HAL:
+      // End of program (Set Halt flag to true)
+      halt = 1;
+      break;
+
+    case NEG:
+      // Negate the register R
+      break;
+
+    case ADD:
+      // Add the registers L and M and store the result
+      // in register R
+      break;
+
+    case SUB:
+      // Subtract register M from register L and store the result
+      // in register R
+      break;
+
+    case MUL:
+      // Multiply registers L and M and store the result
+      // in register R
+      break;
+
+    case DIV:
+      // Divide register L by register M and store the result
+      // in register R
+      break;
+
+    case MOD:
+      // Set register R equal to register L modulo register M
+      break;
+
+    case EQL:
+      // If register L equals register M, set register R to 1.
+      // Otherwise set register R to 0
+      break;
+
+    case NEQ:
+      // If register L does not equal register M, set register R to
+      // 1. Otherwise set register R to 0
+      break;
+
+    case LSS:
+      // If register L is less than register M, set register R to 1.
+      // Otherwise set register R to 0
+      break;
+
+    case LEQ:
+      // If register L is less than or equal to register M, set register R to 1.
+      // Otherwise set register R to 0
+      break;
+
+    case GTR:
+      // If register L is greater than register M, set register R to  1.
+      // Otherwise set register R to 0
+      break;
+
+    case GEQ:
+      // If register L is greater than or equal to register M, set register R to 1.
+      // Otherwise set register R to 0
+      break;
+    }
+  }
   // keep this
   if (printFlag)
   {
